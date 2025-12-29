8510d15afab7e834f38bf5b624171e22a60b214c
Nurtazenov <a.nurtazenov@yandex.ru>
2025-12-29 09:56:21 +0500

    db

---
+++ backend/src/config/orm.datasource.ts
@@ -0,0 +1,23 @@
+import * as dotenv from 'dotenv'
+import migrations from 'src/module/database/migrations';
+import { User } from 'src/user/entities/user.entity';
+import { DataSource } from 'typeorm';
+
+const ENV_FILE = `.env`;
+
+dotenv.config({ path: ENV_FILE });
+
+export default new DataSource({
+    type: 'postgres',
+    host:process.env.DB_HOST,
+    username: process.env.DB_USERNAME,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_DATABASE,
+    port: process.env.DB_PORT as number | undefined,
+    synchronize: false,
+    entities: [User],
+    migrations,
+    migrationsRun: process.env.DB_MIGRATIONS_RUN === 'true',
+    migrationsTableName: process.env.DATABASE_MIGRATIONS_TABLE_NAME
+})
+
